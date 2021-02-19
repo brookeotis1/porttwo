d3674529c654727234f9f8f1db56652f66cffd13
Brooke Maneotis <xmaneotisb@yahoo.com>
2021-02-18 17:37:20 -0700

    try to fix images again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
-
 import Layout from "./components/layout"
+import{ErrorBoundary} from 'react-error-boundary'
+
 // Components
 import Header from "./components/Header"
 import Work from "./components/Work"
@@ -9,8 +10,19 @@ import Skills from "./components/skills"
 import Promotion from "./components/Promotion"
 import Footer from "./components/Footer"
 
+function ErrorFallback({error}) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{color: 'red'}}>{error.message}</pre>
+    </div>
+  )
+}
+
 const App = () => {
 return(
+  <div>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
   <Layout>
     
     <Header></Header>
@@ -20,6 +32,8 @@ return(
     <Promotion></Promotion>
     <Footer></Footer>
   </Layout>
+  </ErrorBoundary>
+  </div>
 )
 }
 export default App
